fc1f471650a69a95dc3c673f918ebe571f071755
Ronn Ross <Ronn.ross@gmail.com>
2017-10-08 08:43:31 -0400

    api end point working

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+var data = require('./mock-data');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('pages/index');
+});
+
+app.get('/api', (req, res) => {
+  res.json(data);
 });
 
 app.listen(3000, () => {
