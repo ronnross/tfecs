6ce945339f155c816d218aaf84984779693407d4
Ronn Ross <Ronn.ross@gmail.com>
2017-10-08 22:32:54 -0400

    --amend

--- src/components/DesignSystem/QuantitySelector.js
+++ src/components/DesignSystem/QuantitySelector.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const QuantitySelector = () => {
   return (
-    <div className="quantity__area">
-      <label htmlFor="quantity">
-        quantity:
+    <label className="quantity__area" htmlFor="quantity">
+      <span className="quantity__title">quantity:</span>
+      <div className="quantity__selection">
         <button className="quantity__button">
           <i className="fa fa-minus quantity__icon" />
         </button>
-        1
+        <span className="quantity__display">1</span>
         <button className="quantity__button">
           <i className="fa fa-plus quantity__icon" />
         </button>
-      </label>
-    </div>
+      </div>
+    </label>
   );
 };
 
